3509a42566cfd9be2e2ec091f39df51e66da80d6
Shubham <dev4fun007@gmail.com>
2021-11-13 10:28:56 +0530

    updated price and fee info in dummy response

--- brokeractions.go
+++ brokeractions.go
@@ -31,8 +31,9 @@ func (receiver BrokerActionCoinDcx) ExecuteMarketOrder(order common.RequestMarke
 		Str("order-type", string(order.OrderType)).
 		Msg("broker action completed")
 
-	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":0.000008,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":0.00001567,"created_at":"%s","updated_at":"%s"}]}`
-	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), order.Quantity, order.Quantity, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
+	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":%f,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":%f,"created_at":"%s","updated_at":"%s"}]}`
+	feeAmount := 0.002 * order.Quantity * order.LastPrice
+	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), feeAmount, order.Quantity, order.Quantity, order.LastPrice, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
 	var orderResponse common.OrderResponse
 	err := json.Unmarshal([]byte(res), &orderResponse)
 	if err != nil {
@@ -51,8 +52,9 @@ func (receiver BrokerActionCoinDcx) ExecuteLimitOrder(order common.RequestLimitO
 		Str("order-type", string(order.OrderType)).
 		Msg("broker action completed")
 
-	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":0.000008,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":0.00001567,"created_at":"%s","updated_at":"%s"}]}`
-	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), order.Quantity, order.Quantity, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
+	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":%f,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":%f,"created_at":"%s","updated_at":"%s"}]}`
+	feeAmount := 0.002 * order.Quantity * order.PricePerUnit
+	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), feeAmount, order.Quantity, order.Quantity, order.PricePerUnit, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
 	var orderResponse common.OrderResponse
 	err := json.Unmarshal([]byte(res), &orderResponse)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/dev4fun007/autobot-broker-coindcx
 go 1.17
 
 require (
-	github.com/dev4fun007/autobot-common v0.0.0-20211105123647-f8b9f0192c69 // indirect
-	github.com/rs/zerolog v1.26.0 // indirect
+	github.com/dev4fun007/autobot-common v0.0.0-20211113044536-2819e9d01cbb
+	github.com/rs/zerolog v1.26.0
 )
