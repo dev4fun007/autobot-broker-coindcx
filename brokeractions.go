062bd9439149d51c45b895a3ce40189e5be6f5bb
Shubham <dev4fun007@gmail.com>
2021-11-14 13:14:33 +0530

    adjusted fee amount

--- brokeractions.go
+++ brokeractions.go
@@ -32,7 +32,7 @@ func (receiver BrokerActionCoinDcx) ExecuteMarketOrder(order common.RequestMarke
 		Msg("broker action completed")
 
 	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":%f,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":%f,"created_at":"%s","updated_at":"%s"}]}`
-	feeAmount := 0.002 * order.Quantity * order.LastPrice
+	feeAmount := 0.001 * order.Quantity * order.LastPrice
 	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), feeAmount, order.Quantity, order.Quantity, order.LastPrice, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
 	var orderResponse common.OrderResponse
 	err := json.Unmarshal([]byte(res), &orderResponse)
@@ -53,7 +53,7 @@ func (receiver BrokerActionCoinDcx) ExecuteLimitOrder(order common.RequestLimitO
 		Msg("broker action completed")
 
 	res := `{"orders":[{"id":"ead19992-43fd-11e8-b027-bb815bcb14ed","market":"%s","order_type":"%s","side":"%s","status":"open","fee_amount":%f,"fee":0.1,"total_quantity":%f,"remaining_quantity":%f,"avg_price":0,"price_per_unit":%f,"created_at":"%s","updated_at":"%s"}]}`
-	feeAmount := 0.002 * order.Quantity * order.PricePerUnit
+	feeAmount := 0.001 * order.Quantity * order.PricePerUnit
 	res = fmt.Sprintf(res, order.Market, string(order.OrderType), string(order.ActionType), feeAmount, order.Quantity, order.Quantity, order.PricePerUnit, time.Now().Format(time.RFC3339), time.Now().Format(time.RFC3339))
 	var orderResponse common.OrderResponse
 	err := json.Unmarshal([]byte(res), &orderResponse)
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/dev4fun007/autobot-broker-coindcx
 go 1.17
 
 require (
-	github.com/dev4fun007/autobot-common v0.0.0-20211113044536-2819e9d01cbb
+	github.com/dev4fun007/autobot-common v0.0.0-20211113165618-1e78a08ac52a
 	github.com/rs/zerolog v1.26.0
 )
